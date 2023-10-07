0dd886f8a5ff61cad5b869092053855073a6f2da
WiShTe <Clava1922007@yandex.ru>
2023-10-07 20:35:01 +0700

    fix F401

--- lyceum/catalog/apps.py
+++
@@ -1,6 +0,0 @@
-
-
-
-class CatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalog'
--- lyceum/homepage/apps.py
+++
@@ -1,5 +0,0 @@
-
-
-class HomepageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homepage'
