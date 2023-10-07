09d71b8caf96c522210257e1b86c82a677a745e3
WiShTe <Clava1922007@yandex.ru>
2023-10-07 20:28:25 +0700

    adjusted it to the tests and add readme.md

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fb1nz@hdrk)vlics=1ut9s42l0t' \
-             '!1&qxq3%jm+gq)''epamv_&x%'
+SECRET_KEY = 'фейковые секретные данные'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
